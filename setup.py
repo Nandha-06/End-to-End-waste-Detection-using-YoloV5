62bbed23092ef6efabb247a5f1c53ddf99c2c771
Nandha kumar <rtnandha2004@gmail.com>
2025-03-18 22:39:36 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup (
+    name = 'wasteDetection',
+    version = '0.0.0',
+    author = 'Nandha Kumar',
+    author_email = 'rtnandha2004@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
